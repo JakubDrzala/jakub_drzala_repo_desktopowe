ed61ff47a2e37f9f3dd8ec0feff2b34bb53b7600
JakubDrzala <jakubdzrzalaptn@gmail.com>
2022-06-08 13:58:22 +0200

    zadanie

--- Kolo/src/main/java/com/mycompany/kolo/Kolo.java
+++ Kolo/src/main/java/com/mycompany/kolo/Kolo.java
@@ -16,6 +16,10 @@ public class Kolo extends javax.swing.JFrame {
         Icon kolo = new ImageIcon("kolko.png");
         Icon brak = new ImageIcon("brak.png");
         int kto = 0;
+        int g1 = 0;
+        int g2 = 0;
+        String gracz1;
+        String gracz2;
 
     /**
      * Creates new form Kolo
@@ -43,8 +47,45 @@ public class Kolo extends javax.swing.JFrame {
     }
     
     public void Winner(){
-        if(jB_1.getIcon()==krzyzyk && jB_3.getIcon()==krzyzyk && jB_3.getIcon()==krzyzyk){
+        if(jB_1.getIcon()==krzyzyk && jB_2.getIcon()==krzyzyk && jB_3.getIcon()==krzyzyk || 
+           jB_1.getIcon()==krzyzyk && jB_4.getIcon()==krzyzyk && jB_7.getIcon()==krzyzyk ||
+           jB_1.getIcon()==krzyzyk && jB_5.getIcon()==krzyzyk && jB_9.getIcon()==krzyzyk ||
+           jB_2.getIcon()==krzyzyk && jB_5.getIcon()==krzyzyk && jB_8.getIcon()==krzyzyk ||
+           jB_4.getIcon()==krzyzyk && jB_5.getIcon()==krzyzyk && jB_6.getIcon()==krzyzyk ||
+           jB_7.getIcon()==krzyzyk && jB_8.getIcon()==krzyzyk && jB_9.getIcon()==krzyzyk ||
+           jB_3.getIcon()==krzyzyk && jB_6.getIcon()==krzyzyk && jB_9.getIcon()==krzyzyk ||
+           jB_3.getIcon()==krzyzyk && jB_5.getIcon()==krzyzyk && jB_7.getIcon()==krzyzyk){
             System.out.println("wygrywa krzyżyk");
+            g1++;
+            jB_1.setEnabled(false);
+            jB_2.setEnabled(false);
+            jB_3.setEnabled(false);
+            jB_4.setEnabled(false);
+            jB_5.setEnabled(false);
+            jB_6.setEnabled(false);
+            jB_7.setEnabled(false);
+            jB_8.setEnabled(false);
+            jB_9.setEnabled(false);
+        }
+        else if(jB_1.getIcon()==kolo && jB_2.getIcon()==kolo && jB_3.getIcon()==kolo || 
+                jB_1.getIcon()==kolo && jB_4.getIcon()==kolo && jB_7.getIcon()==kolo ||
+                jB_1.getIcon()==kolo && jB_5.getIcon()==kolo && jB_9.getIcon()==kolo ||
+                jB_2.getIcon()==kolo && jB_5.getIcon()==kolo && jB_8.getIcon()==kolo ||
+                jB_4.getIcon()==kolo && jB_5.getIcon()==kolo && jB_6.getIcon()==kolo ||
+                jB_7.getIcon()==kolo && jB_8.getIcon()==kolo && jB_9.getIcon()==kolo ||
+                jB_3.getIcon()==kolo && jB_6.getIcon()==kolo && jB_9.getIcon()==kolo ||
+                jB_3.getIcon()==kolo && jB_5.getIcon()==kolo && jB_7.getIcon()==kolo){
+            System.out.println("wygrywa kółko");
+            jB_1.setEnabled(false);
+            jB_2.setEnabled(false);
+            jB_3.setEnabled(false);
+            jB_4.setEnabled(false);
+            jB_5.setEnabled(false);
+            jB_6.setEnabled(false);
+            jB_7.setEnabled(false);
+            jB_8.setEnabled(false);
+            jB_9.setEnabled(false);
+            g2++;
         }
     }
 
@@ -63,6 +104,9 @@ public class Kolo extends javax.swing.JFrame {
         jTF_g1 = new javax.swing.JTextField();
         jTF_g2 = new javax.swing.JTextField();
         jB_start = new javax.swing.JButton();
+        jF_wyniki = new javax.swing.JFrame();
+        jL_gracz1 = new javax.swing.JLabel();
+        jL_gracz2 = new javax.swing.JLabel();
         jB_1 = new javax.swing.JButton();
         jB_4 = new javax.swing.JButton();
         jB_7 = new javax.swing.JButton();
@@ -75,6 +119,7 @@ public class Kolo extends javax.swing.JFrame {
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jM_nowagra = new javax.swing.JMenuItem();
+        jM_rewanz = new javax.swing.JMenuItem();
         jM_wyniki = new javax.swing.JMenuItem();
 
         jF_Start.setTitle("Podaj nazwę graczy");
@@ -109,7 +154,7 @@ public class Kolo extends javax.swing.JFrame {
                         .addComponent(jLabel2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jTF_g2, javax.swing.GroupLayout.PREFERRED_SIZE, 199, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(16, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addComponent(jB_start, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         jF_StartLayout.setVerticalGroup(
@@ -124,7 +169,36 @@ public class Kolo extends javax.swing.JFrame {
                     .addComponent(jLabel2)
                     .addComponent(jTF_g2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addComponent(jB_start, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jB_start, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jF_wyniki.setMaximumSize(new java.awt.Dimension(300, 174));
+        jF_wyniki.setMinimumSize(new java.awt.Dimension(300, 174));
+
+        jL_gracz1.setFont(new java.awt.Font("sansserif", 0, 24)); // NOI18N
+        jL_gracz1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+
+        jL_gracz2.setFont(new java.awt.Font("sansserif", 0, 24)); // NOI18N
+        jL_gracz2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+
+        javax.swing.GroupLayout jF_wynikiLayout = new javax.swing.GroupLayout(jF_wyniki.getContentPane());
+        jF_wyniki.getContentPane().setLayout(jF_wynikiLayout);
+        jF_wynikiLayout.setHorizontalGroup(
+            jF_wynikiLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jF_wynikiLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jF_wynikiLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jL_gracz1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jL_gracz2, javax.swing.GroupLayout.DEFAULT_SIZE, 288, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        jF_wynikiLayout.setVerticalGroup(
+            jF_wynikiLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jF_wynikiLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jL_gracz1, javax.swing.GroupLayout.DEFAULT_SIZE, 73, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jL_gracz2, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
 
@@ -214,7 +288,20 @@ public class Kolo extends javax.swing.JFrame {
         });
         jMenu1.add(jM_nowagra);
 
+        jM_rewanz.setText("Rewanż");
+        jM_rewanz.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jM_rewanzActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jM_rewanz);
+
         jM_wyniki.setText("Wyniki");
+        jM_wyniki.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jM_wynikiActionPerformed(evt);
+            }
+        });
         jMenu1.add(jM_wyniki);
 
         jMenuBar1.add(jMenu1);
@@ -270,7 +357,7 @@ public class Kolo extends javax.swing.JFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jB_8, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jB_9, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(81, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -377,6 +464,7 @@ public class Kolo extends javax.swing.JFrame {
             kto = 0;
         }
        jB_8.setEnabled(false);
+       Winner();
     }//GEN-LAST:event_jB_8ActionPerformed
 
     private void jB_9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jB_9ActionPerformed
@@ -419,7 +507,47 @@ public class Kolo extends javax.swing.JFrame {
        jB_9.setIcon(brak);
        
        jF_Start.setVisible(false);
+       kto = 0;
+       
+       gracz1 = jTF_g1.getText();
+       gracz2 = jTF_g2.getText();
+       g1 = 0;
+       g2 = 0;
     }//GEN-LAST:event_jB_startActionPerformed
+
+    private void jM_rewanzActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jM_rewanzActionPerformed
+        jB_1.setEnabled(true);
+       jB_2.setEnabled(true);
+       jB_3.setEnabled(true);
+       jB_4.setEnabled(true);
+       jB_5.setEnabled(true);
+       jB_6.setEnabled(true);
+       jB_7.setEnabled(true);
+       jB_8.setEnabled(true);
+       jB_9.setEnabled(true);
+       
+       jB_1.setIcon(brak);
+       jB_2.setIcon(brak);
+       jB_3.setIcon(brak);
+       jB_4.setIcon(brak);
+       jB_5.setIcon(brak);
+       jB_6.setIcon(brak);
+       jB_7.setIcon(brak);
+       jB_8.setIcon(brak);
+       jB_9.setIcon(brak);
+       kto = 0;
+    }//GEN-LAST:event_jM_rewanzActionPerformed
+    
+    public void Wyniki(){
+        jL_gracz1.setText("Nazwa: "+gracz1+" | "+"Wynik: "+g1);
+        jL_gracz2.setText("Nazwa: "+gracz2+" | "+"Wynik: "+g2);
+    }
+    
+    private void jM_wynikiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jM_wynikiActionPerformed
+        jF_wyniki.setVisible(true);
+        jF_wyniki.setSize(300, 250);
+        Wyniki();
+    }//GEN-LAST:event_jM_wynikiActionPerformed
 
     /**
      * @param args the command line arguments
@@ -468,9 +596,13 @@ public class Kolo extends javax.swing.JFrame {
     private javax.swing.JButton jB_9;
     private javax.swing.JButton jB_start;
     private javax.swing.JFrame jF_Start;
+    private javax.swing.JFrame jF_wyniki;
+    private javax.swing.JLabel jL_gracz1;
+    private javax.swing.JLabel jL_gracz2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JMenuItem jM_nowagra;
+    private javax.swing.JMenuItem jM_rewanz;
     private javax.swing.JMenuItem jM_wyniki;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuBar jMenuBar1;
